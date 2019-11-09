7ba8c2f8e4e74ffec05e1ba32cd959378680f102
Joe Giuffrida <giuffridajoe@gmail.com>
2019-11-09 11:15:08 -0500

    updates

---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector: 'app-server-element',
+    templateUrl: ' ',
+    styles: [ ]
+})
+
+export class ServerElementComponent {
+
+}

3cca772e0ba999cf57b41826386bf962c6aefef8
Joe Giuffrida <giuffridajoe@gmail.com>
2019-11-13 06:26:28 -0500

    updates

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'app-cockpit',
@@ -9,17 +9,36 @@ export class CockpitComponent implements OnInit {
     // EventEmitter is a generic type (<>) allowing for custom events to be emitted
     @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
     @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
-    newServerName = '';
-    newServerContent = '';
+    // this is the name of a local reference to be used in this typescript class
+    // you can also pass the name of a component to get access to the first occurence of that component in the app component
+    @ViewChild('serverContentInput', {static: true}) serverContentInput: ElementRef;
 
     constructor() {}
+    /**  this is a lifecycle hook. lifecycle phases:
+     * 1. ngOnChanges: 
+     *    can be executed multiple times.
+     *    executed at the start when a component is created.
+     *    also called when a bound input property (properties decorated with @Input) changes.
+     * 
+     * 2. ngOnInit:
+     *    gets executed when the component is initialized
+     *    hasnt been displayed yet, but Angular finished basic initialization. properties can be accessed,         object has been created etc.
+     *    will run after the constuctor.
+     *  
+     * 3. ngDoCheck:
+     *    will also run multiple times
+     *    will run whenever change detection runs. (system by which Angular determines whether something           changed).
+    */
     ngOnInit() {}
 
-    onAddServer() {
-      this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+    onAddServer(nameInput: HTMLInputElement) {
+      // the nativeElementProperty gives direct access to the underlying element in the template
+      // this.serverContentInput.nativeElement.value = 'Something' - should not do this. instead use string interpolation or property binding
+      console.log(nameInput.value);
+      this.serverCreated.emit({serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value});
     }
 
-    onAddBlueprint() {
-      this.blueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
+    onAddBlueprint(nameInput: HTMLInputElement) {
+      this.blueprintCreated.emit({serverName: nameInput.value, serverContent: this.serverContentInput.nativeElement.value});
     }
 }
